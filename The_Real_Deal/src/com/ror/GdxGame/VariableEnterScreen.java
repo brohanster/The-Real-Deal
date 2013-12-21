23e2c15f8f406ce5a720edc9820278224cf62963
brohanster <yadav.medhi@gmail.com>
2013-12-21 18:23:24 -0500

    worked a little more on the variable screen, not much tho

--- The_Real_Deal/src/com/ror/GdxGame/VariableEnterScreen.java
+++ The_Real_Deal/src/com/ror/GdxGame/VariableEnterScreen.java
@@ -1,16 +1,35 @@
 package com.ror.GdxGame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 public class VariableEnterScreen implements Screen{
 
 	final GdxGame game;
+	Stage stage;
+	Skin s;
+	Label objectLabel, velocityLabel, angleLabel, planetLabel, resistanceLabel;
+	public static TextField velocityField, angleField;
+	public static CheckBox resistanceBox;
+	public static SelectBox objectBox, planetBox;
 	public VariableEnterScreen(final GdxGame gam){
 		game = gam;
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		Table table = new Table();
+		table.setFillParent(true);
+		s = new Skin(Gdx.files.internal("uiskin.json"));
+		
 	}
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
 		
 	}
 
