b896d804d044c2b34184b9a8e812ce3dd0c6f03e
brohanster <yadav.medhi@gmail.com>
2013-12-26 19:47:26 -0500

    worked a little more

--- The_Real_Deal/src/com/ror/GdxGame/physicsChallenge/ChallengeChoiceScreen.java
+++ The_Real_Deal/src/com/ror/GdxGame/physicsChallenge/ChallengeChoiceScreen.java
@@ -1,12 +1,18 @@
 package com.ror.GdxGame.physicsChallenge;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.ror.GdxGame.GdxGame;
+import com.ror.GdxGame.MainMenuScreen;
 
 public class ChallengeChoiceScreen implements Screen{
 
@@ -16,13 +22,76 @@ public class ChallengeChoiceScreen implements Screen{
 	Stage stage;
 	Table table;
 	TextButton backButton, angleChoice, velocityChoice, gravityChoice, distanceChoice;//We can add more, but ill leave it at this for now
-	
+	SpriteBatch batch;
+	Texture background;
 	public ChallengeChoiceScreen(final GdxGame gam){
 		game = gam;
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		table = new Table();
+		s = new Skin(Gdx.files.internal("uiskin.json"));
+		challengeChoice = new Label("Which challenge would you like to take?", s);
+		batch = new SpriteBatch();
+		Texture.setEnforcePotImages(false);
+		background = new Texture(Gdx.files.internal("wallpaper.jpg"));
+		backButton = new TextButton("Back", s);
+		backButton.addListener(new InputListener(){
+        	public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+        		game.setScreen(new MainMenuScreen(game));
+        		return true;
+        	}
+        });
+		angleChoice = new TextButton("Angle Challenge", s);
+		angleChoice.addListener(new InputListener(){
+        	public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+        		game.setScreen(new ChallengeDisplayScreen(game));
+        		return true;
+        	}
+        });
+		velocityChoice = new TextButton("Velocity Challenge", s);
+		velocityChoice.addListener(new InputListener(){
+        	public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+        		game.setScreen(new ChallengeDisplayScreen(game));
+        		return true;
+        	}
+        });
+		gravityChoice = new TextButton("Gravity Challenge", s);
+		gravityChoice.addListener(new InputListener(){
+        	public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+        		game.setScreen(new ChallengeDisplayScreen(game));
+        		return true;
+        	}
+        });
+		distanceChoice = new TextButton("Distance Challenge", s);
+		distanceChoice.addListener(new InputListener(){
+        	public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+        		game.setScreen(new ChallengeDisplayScreen(game));
+        		return true;
+        	}
+        });
+		table.setFillParent(true);
+		table.setSkin(s);
+		table.center();
+		table.add(challengeChoice);
+		table.row();
+		table.add(" ").height(75);
+		table.row();
+		table.add(angleChoice).height(75).width(250);
+		table.row();
+		table.add(velocityChoice).height(75).width(250);
+		table.row();
+		table.add(gravityChoice).height(75).width(250);
+		table.row();
+		table.add(distanceChoice).height(75).width(250);
+		stage.addActor(table);
 	}
 	@Override
 	public void render(float delta) {
-		
+		batch.begin();
+		batch.draw(background, 0,0);
+		batch.end();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();  
 	}
 
 	@Override
--- The_Real_Deal/src/com/ror/GdxGame/physicsChallenge/ChallengeDisplayScreen.java
+++ The_Real_Deal/src/com/ror/GdxGame/physicsChallenge/ChallengeDisplayScreen.java
@@ -1,5 +1,54 @@
 package com.ror.GdxGame.physicsChallenge;
 
-public class ChallengeDisplayScreen {
+import com.badlogic.gdx.Screen;
+import com.ror.GdxGame.GdxGame;
+
+public class ChallengeDisplayScreen implements Screen{
+
+	final GdxGame game;
+	public ChallengeDisplayScreen(final GdxGame gam){
+		game = gam;
+	}
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
